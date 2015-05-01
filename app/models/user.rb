0db90e4b666868b5e4effae52677e2cfe513ad67
Travis <dellybro@gmail.com>
2015-05-01 00:23:09 -0700

    Fixed followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
 
   #Avatars
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "25x25>" }, :default_url => "rails.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => '50x50>' }, :default_url => "rails.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   #Avatars are supser fuckign easy
 
