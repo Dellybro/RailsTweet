a407b3cfc02fafdbe4c1d548a9f89e983e7285e7
Travis <dellybro@gmail.com>
2015-05-01 22:10:06 -0700

    paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,8 +3,6 @@ if Rails.env.production?
 	  		:storage => :s3,
   			:s3_credentials => {
     		:bucket => 'twitterclone-assets-users'
-			:aws_access_key_id		=> ENV['aws_access_key_id'],
-			:aws_secret_access_key 	=> ENV['aws_secret_access_key']
   		}
 	}
 end
