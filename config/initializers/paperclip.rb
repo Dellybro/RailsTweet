798d7371997a35c1f94126a9fe07ca79e60923cf
Travis <dellybro@gmail.com>
2015-05-04 22:41:53 -0700

    paperclip fix

--- config/initializers/paperclip.rb
+++
@@ -1,4 +0,0 @@
-if Rails.env.production?
-	Paperclip::Attachment.default_options[:url] = 'twitterclone-assets-users.amazonaws.com'
-	Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-end
